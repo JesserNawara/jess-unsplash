f5d4098795e7faa48ddea7306221affaeac42c5e
jess <jasservaldi@gmail.com>
2021-08-07 15:17:38 +0100

    unsplashed 99% finished

--- server/index.js
+++ server/index.js
@@ -1,23 +1,23 @@
 const express = require('express')
 const app = express()
-const port = 3002
+const port = 3001
 const db = require('../database');
-const fileUpload = require('express-fileupload')
-// const multer = require('multer');
+
+const multer = require('multer');
 const cors = require('cors');
 const cloudinary = require('cloudinary').v2;
-const path = require('path');
-app.use(fileUpload());
+
+
 app.use(express.json())
 app.use(express.static(__dirname + "/../build"));
 app.use(express.urlencoded({ extended: true }));
+
 app.use(cors({
   origin: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 }))
-app.use('/', express.static(path.join(__dirname, '/')));
-
+   
 cloudinary.config({ 
   cloud_name: 'jessssss', 
   api_key: '249939786819951', 
@@ -25,82 +25,19 @@ cloudinary.config({
   secure: true 
 });
 
-app.post('/upload', (req, res) => {
-  if(req.files === null){
-    return res.status(400).json({msg: "no files were uploaded"})
-  }
-  const file = req.files.file;
-  console.log(file);
-  cloudinary.uploader.upload(file, (result, error) => {
-    if(result){
-      res.status(200).json(result);
-    } else {
-      res.status(500).json(error);
-    }
-  })
-})
-
-// app.post("/api/image", upload.single('image'), (req, res, err) => {
-//   if (!req.file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF)$/)) {
-//     res.send({ msg:'Only image files (jpg, jpeg, png) are allowed!'})};
-//     const image = req.file.filename;
-//     const sqlInsert = "UPDATE images SET `image` = ? WHERE id = ?";
-
-//     connection.query(sqlInsert, [image, id], (err, result) => {
-//       if(err){
-//         console.log(err);
-//         res.send({
-//           msg: err
-//         })
-//       }
-//       if (result) {
-//         res.send({ 
-//           data: result,
-//           msg: 'Your image has been updated successfully!'
-//         })
-//       }
-//     })
-// })
-
-// const storage = multer.diskStorage({
-//   destination: (req, file, cb) => {
-//     cb(null, "./")},
-//     filename: function(req, file, cb) {
-//       const ext = file.mimetype.split("/")[1];
-//       cb(null, `uploads/${file.originalname}-${Date.now()}.${ext}`);
-//     }
-// });
-
-// const upload =multer({
-//   storage: storage
-// })
-
+//___________________________FETCH_ITEMS_________________________
 app.get('/data', function (req, res) {
   const items = req.body.items
   db.query('SELECT * FROM items ORDER BY id DESC',items ,(err, result)=>{
     if (err) {
-      res.statusCode(400).send(err);
+      res.status(400).send(err);
     } else {
       res.send(result);
     }
   })
 })
 
-app.post('/add/item', function(req, res){
-  const image = req.body.image
-  const label = req.body.label;
-  db.query('INSERT INTO items (image,label) VALUES (?,?)', 
-    [image, label],
-  (err, result) => {
-    if (err) {
-      res.status(400).send(err);
-    } else {
-      res.status(200).send(result);
-    }
-  }
-  )
-})
-
+//_________________________DELETE_ITEM___________________________
 app.delete('/item/delete:id', function (req, res){
   const id = req.params.id
   db.query('DELETE FROM items WHERE id = ?',id, 
@@ -111,6 +48,46 @@ app.delete('/item/delete:id', function (req, res){
       res.status(200).send(result);
     }
   })
+})
+
+//__________________________MULTER_STORAGE________________________
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "./")},
+    filename: function(req, file, cb) {
+      const ext = file.mimetype.split("/")[1];
+      cb(null, `uploads/${file.originalname}-${Date.now()}.${ext}`);
+    }
+});
+
+const upload =multer({ storage: storage })
+
+//___________________________________UPLOAD_IMAGE__________________
+app.post("/upload", upload.single('image'), (req, res, err) => {
+  if (!req.file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF)$/)) {
+    res.send({ msg:'Only image files (jpg, jpeg, png) are allowed!'})};
+
+    const image = req.file.filename;
+
+    cloudinary.uploader.upload(image, (error, response) => {
+      const sqlInsert = "INSERT INTO items (image, label) VALUES(?,?)";
+      const label = req.body.label
+      const image = response.secure_url;
+      db.query(sqlInsert, [image, label], (err, result) => {
+        if(err){
+          console.log(err);
+          res.send({
+            msg: "AAAAAA sth went wrong bruv"+err
+          })
+        }
+        if (result) {
+          res.send({ 
+            data: result,
+            msg: 'Your image has been updated successfully!'
+          })
+        }
+      })
+    })
 })
 
 app.listen(port, () => {
--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -1,54 +1,23 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 
 export default function Create() {
     const [label, setLabel] = useState("");
     const [image, setImage] = useState("");
     const [uploadedImage, setUploadedImage] = useState("");
-    const [checked, setChecked] = useState(false);
-    const [message, setMessage] = useState('');
-    const [confirmColor, setConfirmColor] = useState('');
-
-    useEffect(()=> {
-        confirmMsg()
-    })
-
-    const addItem= ()=>{
-        axios.post('/add/item', {image, label})
-        .then(result => {
-         console.log(result.data);
-        }).catch(error=>{
-            console.log(error);
-        })
-    }
-
-    const confirmMsg = ()=>{
-        if( checked === true ){
-            setMessage('check complete!')
-            setConfirmColor('green');
-        } else if( checked === false ){
-            setMessage('confirm picture please')
-            setConfirmColor('red');
-        }
-    }
 
     const uploadImage= (e)=>{
         e.preventDefault();
         const formData = new FormData();
-        formData.append('file', uploadedImage);
+        formData.append('image', uploadedImage);
+        formData.append('label', label);
         formData.append('upload_preset', "jessStorage");
-     
-        axios.post("https://api.cloudinary.com/v1_1/jessssss/image/upload",
-        formData, {headers:{
-            "content-type": "multipart/form-data"
-        }}
-        ).then(response => {
-            setImage(response.data.secure_url)
-            setChecked(true)
-        }).catch(error => {
-            console.log(error);
-            setChecked(false);
-        })
+        console.log(uploadedImage);
+
+      axios.post('/upload', formData).then((result)=>{
+          console.log(result);
+      })
+        
     }
 
     return (
@@ -61,16 +30,8 @@ export default function Create() {
             /> <br />
             <input id="file" type="file"  onChange={(event)=>{setUploadedImage(event.target.files[0])}}/>
             <br />
-            <button id="checkBtn" style={{"backgroundColor":confirmColor}} onClick={uploadImage}> {message} </button> 
-            <br />
             <button id="cancelBtn" onClick={()=>{props.toggle===false}} >Cancel</button>
-            <button id="submitBtn"  onClick={()=>{
-                if(checked === true) {
-                    addItem()
-                } else {
-                    alert("please confirm your image!")
-                }
-            }}>Submit</button>
+            <button id="submitBtn"  onClick={uploadImage}>Submit</button>
             </form>
         </div>
     )
